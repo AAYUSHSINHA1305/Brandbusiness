c645ebc9ff2dd365384208bdeb7e09bb3ce985fb
arvindkumar0205 <arvindkumar1281.ak@gmail.com>
2022-10-20 16:59:12 +0530

    homepage

--- lib/pages/quote.dart
+++ lib/pages/quote.dart
@@ -1,3 +1,4 @@
+import 'package:brandbusiness/util/hex_color.dart';
 import 'package:flutter/material.dart';
 
 class Quote extends StatefulWidget {
@@ -10,15 +11,271 @@ class Quote extends StatefulWidget {
 class _QuoteState extends State<Quote> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Image.asset(
-              'assets/logo.png',
-            ),
-            Image.asset("assets/bnr1.jpg"),
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                height: size.height * 0.1,
+              ),
+              Stack(
+                children: [
+                  Image.asset(
+                    "assets/bnr1.jpg",
+                  ),
+                  Positioned(
+                    top: 30,
+                    left: 20,
+                    child: Text(
+                      "Request A Demo",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "Newzealand Office",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "98 Tiverton Road, New Windsor, Auckland 0600\n Call us at : +64 09 889 4212",
+                  style: TextStyle(fontSize: 15, color: blueclor, height: 1.5),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "Australia Office",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "Unit 18 1 Browne Pde, Warwick Farm, NSW, 2170 \n Call us at : +61 02 8006 4424",
+                  style: TextStyle(fontSize: 15, color: blueclor, height: 1.5),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "Canada Office",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "115 Bellchase Trail, Brampton, ON L6P 3L3 \n Call us at : +1 814 822 0433",
+                  style: TextStyle(fontSize: 15, color: blueclor, height: 1.5),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "USA Office",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "316 Quince Ave Galloway, NJ 08205 \n Call us at : +1 814 822 0433",
+                  style: TextStyle(fontSize: 15, color: blueclor, height: 1.5),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "London Office",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "27 Old Gloucester Street LONDON WC1N 3AX UNITED KINGDOM\nCall us at : +44 20 3239 4243",
+                  style: TextStyle(fontSize: 15, color: blueclor, height: 1.5),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "First Name *",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Last Name *",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Last Name *",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Company Name ",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ExpansionTile(
+                title: Text(
+                  'Select Ope',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
